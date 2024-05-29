0eabe15bd96f865593ce7406c4e954bfef14bdf3
Chamber6821 <vania.osinin@yandex.ru>
2024-05-29 05:30:09 +0300

    feat(server): use stdout to send messages to client

--- src/cmd/server.c
+++ src/cmd/server.c
@@ -16,27 +16,40 @@ void onInt(int signal) {
   work = 0;
 }
 
-int handleCommand(const char *command, char *buffer) {
-#define PRINT(...) buffer += sprintf(buffer, __VA_ARGS__)
-  if (strcmp(command, "HI\n") == 0) {
-    PRINT("Hi!\n");
-    return 0;
-  }
-  if (strcmp(command, "QUIT\n") == 0) {
-    PRINT("Bye-bye\n");
+int startsWith(const char *prefix, const char *str) {
+  return strncmp(str, prefix, strlen(prefix)) == 0;
+}
+
+const char *tail(const char *command) {
+  char *offset = strchr(command, ' ');
+  if (offset == NULL) return command + strlen(command) - 1;
+  return offset + 1;
+}
+
+int handleCommand(const char *command) {
+  if (strcmp("\n", command) == 0) {
+    /* nothing */
+  } else if (startsWith("ECHO", command)) {
+    printf("%s", tail(command));
+  } else if (startsWith("HI", command) == 0) {
+    printf("Hi!\n");
+  } else if (startsWith("QUIT", command) == 0) {
     return -1;
+  } else {
+    printf("Unknown command: %s", command);
   }
-  PRINT("Unknown command: %s", command);
   return 0;
 }
 
 int runShell(int connection) {
+  dup2(connection, STDOUT_FILENO);
   while (1) {
+    const char prefix[] = "> ";
+    write(connection, prefix, sizeof(prefix) - 1);
     char command[1024] = {0};
     if (read(connection, command, sizeof(command)) == -1) ER("Failed to read");
-    char output[1024] = {0};
-    if (handleCommand(command, output) == -1) break;
-    write(connection, output, strlen(output));
+    fprintf(stderr, "Connection: %d Command: %s", connection, command);
+    if (handleCommand(command) == -1) break;
   }
   return 0;
 }
