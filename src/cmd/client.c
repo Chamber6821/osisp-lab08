3d989b539cdd4b210e1c33440498d8fbaf8834fd
Chamber6821 <vania.osinin@yandex.ru>
2024-05-29 05:29:10 +0300

    feat(client): use different thread for async behavior

--- src/cmd/client.c
+++ src/cmd/client.c
@@ -1,41 +1,71 @@
+#include "onSignal.h"
 #include "utils.h"
+#include <bits/pthreadtypes.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <time.h>
 #include <unistd.h>
 
-int main() {
-  int tcpSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (connect(tcpSocket, &ADDRESS, sizeof(ADDRESS)) == -1)
-    ER("Failed to connect");
+int tcpSocket = 0;
+pthread_t transmit = 0, display = 0;
 
-  char buffer[1024] = {0};
-  ssize_t bytes_read, bytes_written;
+void onInt(int signal) {
+  (void)signal;
+  pthread_cancel(transmit);
+  pthread_cancel(display);
+}
 
+void *displayAll(void *arg) {
+  (void)arg;
   while (1) {
-    printf("> ");
-    fflush(stdout);
-    fgets(buffer, sizeof(buffer), stdin);
-    if (strlen(buffer) == 1) continue;
-
-    bytes_written = write(tcpSocket, buffer, strlen(buffer));
-    if (bytes_written == -1) {
-      perror("write");
-      break;
-    }
-
-    bytes_read = read(tcpSocket, buffer, sizeof(buffer) - 1);
-    if (bytes_read == -1) {
+    char buffer[1024] = {0};
+    int got = read(tcpSocket, buffer, sizeof(buffer) - 1);
+    if (got == -1) {
       perror("read");
       break;
     }
-    if (bytes_read == 1 && buffer[0] == 0) break;
-    buffer[bytes_read] = '\0';
+    if (got == 1 && buffer[0] == 0) break;
+    buffer[got] = '\0';
     printf("%s", buffer);
+    fflush(stdout);
   }
+  pthread_cancel(transmit);
+  return NULL;
+}
 
+void *transmitAll(void *arg) {
+  (void)arg;
+  while (1) {
+    char buffer[1024] = {0};
+    fgets(buffer, sizeof(buffer), stdin);
+    int sent = write(tcpSocket, buffer, strlen(buffer));
+    if (sent == -1) {
+      perror("write");
+      break;
+    }
+  }
+  pthread_cancel(display);
+  return NULL;
+}
+
+int main() {
+  onSignal(SIGINT, onInt);
+  tcpSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (connect(tcpSocket, &ADDRESS, sizeof(ADDRESS)) == -1)
+    ER("Failed to connect");
+
+  pthread_create(&display, NULL, displayAll, NULL);
+  pthread_create(&transmit, NULL, transmitAll, NULL);
+  pthread_join(transmit, NULL);
+  pthread_join(display, NULL);
+
+  const char quit[] = "QUIT\n";
+  write(tcpSocket, quit, sizeof(quit) - 1);
   close(tcpSocket);
   printf("Bye, bye!\n");
 }
